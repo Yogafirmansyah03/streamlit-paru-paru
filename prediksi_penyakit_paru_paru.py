f656572aafcfdcbc78bb36da960f585b77eddcb4
Yogafirmansyah03 <101878398+Yogafirmansyah03@users.noreply.github.com>
2025-04-29 00:17:48 +0700

    Update prediksi_penyakit_paru_paru.py

--- prediksi_penyakit_paru_paru.py
+++ prediksi_penyakit_paru_paru.py
@@ -28,6 +28,7 @@ df = pd.read_csv('predic_tabel.csv')
 
 """**EDA**"""
 import seaborn as sns
+import matplotlib.pyplot as plt
 plt.figure(figsize=(12, 6))
 sns.boxplot(data=df)
 plt.title("Boxplot Distribusi Data")
@@ -47,7 +48,6 @@ lifestyle_features = ['Usia', 'Jenis_Kelamin', 'Merokok', 'Aktivitas_Begadang', 
 
 # Loop untuk setiap fitur
 from statsmodels.stats.outliers_influence import variance_inflation_factor
-import matplotlib.pyplot as plt
 for feature in lifestyle_features:
     plt.figure(figsize=(6, 4))
 
